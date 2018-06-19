69c28776c5d4e9294da0b12b6780a4e224d87948
OzyOzk <38989289+Ozan192@users.noreply.github.com>
2018-06-19 22:55:15 +0100

    Arrays no longer grow and are fixed size of 500 elements

--- oguplot.py
+++ oguplot.py
@@ -22,11 +22,16 @@ targetsn = "9553034373435110E020"
 ports = serial.tools.list_ports.comports()
 
 for p in ports:
-	print (p.serial_number)
-	if p.serial_number == 9553034373435110E020:
-		p.device
+  print (p.serial_number)
+  if p.serial_number == 9553034373435110E020:
+    p.device
+
 
 ser = serial.Serial(p.device, 9600, timeout=1)
+if not ser.isOpen():
+  ser.open()
+print('com3 is open', ser.isOpen())
+
 
 app = QtGui.QApplication([])
 
@@ -41,27 +46,29 @@ curve1 = p6.plot(pen='y')
 
 curve2 = p6.plot(pen='g')
 
-data1 = [0]
-data2 = [0]
+data1 = [0] * 500
+data2 = [0] * 500
 x=0
 def update():
-    global curve1, curve2, data1, data2, x
-    line = ser.readline()
-    csv = line.decode().split(',')
-    if len(csv) == 2:	    
-	    set1 = csv[0]
-	    set2 = csv[1]
-	    data1.append(float(set1))
-	    data2.append(float(set2))
-	    print (len(data1), len(data2))
-	    xdata1 = np.array(data1[-500:], dtype='float32')
-	    xdata2 = np.array(data2[-500:], dtype='float32')
-	    curve1.setData(xdata1)
-	    x += 1
-	    curve1.setPos(x, 0)
-	    curve2.setData(xdata2)
-	    curve2.setPos(x, 0)
-	    app.processEvents()
+  global curve1, curve2, data1, data2, x
+  line = ser.readline()
+  csv = line.decode().split(',')
+  if len(csv) == 2:	    
+    set1 = csv[0]
+    set2 = csv[1]
+    data1.append(float(set1))
+    data2.append(float(set2))
+    data1.pop(0)
+    data2.pop(0)
+    print (len(data1), len(data2))
+    xdata1 = np.array(data1[-500:], dtype='float32')
+    xdata2 = np.array(data2[-500:], dtype='float32')
+    curve1.setData(xdata1)
+    x += 1
+    curve1.setPos(x, 0)
+    curve2.setData(xdata2)
+    curve2.setPos(x, 0)
+    app.processEvents()
 
 
 timer = QtCore.QTimer()
@@ -70,9 +77,9 @@ timer.start(0)
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
-    import sys
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+  import sys
+  if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+    QtGui.QApplication.instance().exec_()
 
 '''
 	print p.vid
--- serialtest.py
+++ serialtest.py
@@ -4,15 +4,15 @@ controller = serial.Serial
 ports = serial.tools.list_ports.comports()
 
 for p in ports:
-    print(p.serial_number)
-    if p.serial_number == 9553034373435110E020:
-        p.device
+  print(p.serial_number)
+  if p.serial_number == 9553034373435110E020:
+    p.device
 
 ser = serial.Serial(p.device, 9600)
 
 while(True):
-    line = ser.readline()
-    #print(line)
-    first = line.decode().split(",")[0]
-    secon = line.decode().split(",")[1]
-    print(first, secon)
+  line = ser.readline()
+  #print(line)
+  first = line.decode().split(",")[0]
+  secon = line.decode().split(",")[1]
+  print(first, secon)

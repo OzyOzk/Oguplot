853c6e3d41f17462ed02d11c8ca2784ba7421421
OzyOzk <38989289+Ozan192@users.noreply.github.com>
2018-12-13 12:49:12 +0000

    added auto plot count
    automatically detects up to three plots and plos them

--- oguplot.py
+++ oguplot.py
@@ -3,6 +3,7 @@ from PyQt5 import QtGui, QtCore
 import serial.tools.list_ports
 import pyqtgraph as pg
 import numpy as np
+from numpy import zeros
 import sys
 import math
 """
@@ -11,30 +12,25 @@ Created on Thu Jan 18 14:38:46 2018
 @author: OzyOzk
 """
 
-"""
-If you are on a windows machine, after succesfully running the code once, if you
-terminated and run again, you will get the following error
-
-SerialException: could not open port 'COM3': PermissionError(13, 'Access is denied.', None, 5)
-
-You need to either reset your Kernel or delete all environment variables. On Spyder,
-this can be done in the Ipython console. Reset is inside the options menu on the top 
-right of the console (Cog symbol). Remove all variables is to the left of the Cog by
-the stop button.
-
-Once I find a proper solution I will update the code. 
-"""
-
-
 ser = None
-dt = 5  # Time delta in milliseconds
+dt = 2  # Time delta in milliseconds
 element_count = 0
-curves = list()
-curve_xdata = list()
 
-size = 500
-buffersize = 2*500
+view = 500
+buffersize = 2*view  # it's a double buffer
+max_plot_count = 3
+
+sample_size = 3
+
+colours = ['r', 'g', 'b']
+plot_names = ['Data 1', 'Data 2', 'Data 3']
+
 #serial_number = "9553034373435110E020"
+
+
+# to contain the information for the plots
+buffers = zeros((max_plot_count, buffersize+1), int)
+curve_container = list()
 
 
 def find_device(sn):
@@ -44,7 +40,7 @@ def find_device(sn):
             comport = p.device
             print("Found device on", p.device)
             return comport
-    return "Not found"
+    return '!'
 
 
 def connect_to_device(comport):
@@ -56,39 +52,46 @@ def poll_button(lineedit):
 
 
 def find_length(ser):
+    global sample_size
     elements = 0
-    for x in range(5):
+    for x in range(sample_size):
         line = ser.readline()
         csv = line.decode().split(',')
         elements += len(csv)
-    return elements/5
+    return elements/sample_size
+
+
+def make_curves(elements):
+    global curve_container, colours, plot_names
+    for x in range(elements):
+        curve_container.append(
+            p1.plot(pen=colours[x], name=plot_names[x], width=0.01))
 
 
 def qlewrapper():
-    global element_count
+    global element_count, ser
     serial_number = poll_button(t1)
     comport = find_device(serial_number)
-    if comport == "Not found":
+    if comport == '!':
         return
-    global ser
     ser = connect_to_device(comport)
-    print(comport)
     element_count = int(math.ceil(find_length(ser)))
-    print("There are", element_count, "values in the CSV")
+    print(element_count)
+    make_curves(element_count)
 
 
-def make_curves(x, px):
-    global element_count, curves, curve_xdata, buffersize
-    for x in range(element_count):
-        curves[x] = px.plot()
-        curve_xdata[x] = np.zeros(buffersize+1, int)
-
-
-def shift_elements(buffer, csv):
-    global size, buffersize, element_count
+def shift_plot(buffer, csv):
+    global view, buffersize, element_count
     i = buffer[buffersize]
-    buffer[i] = buffer[i+size] = csv[0]
-    buffer[buffersize] = i = (i+1) % size
+    buffer[i] = buffer[i+view] = csv
+    buffer[buffersize] = i = (i+1) % view
+    return i
+
+
+def set_data(curve_container, buffer, element):
+    global view, x
+    curve_container.setData(buffer[element:element+view])
+    curve_container.setPos(x, 0)
 
 
 def close_port():
@@ -100,6 +103,19 @@ def close_port():
 def close_app():
     close_port()
     sys.exit()
+
+
+def update():
+    global x, view, element_count, curve_container, buffers, buffersize
+    if(ser != None):
+        line = ser.readline()
+        csv = line.decode().split(',')
+        x += 1
+        if len(csv) == element_count:
+            for el in range(element_count):
+                set_data(curve_container[el], buffers[el],
+                         shift_plot(buffers[el], csv[el]))
+        app.processEvents()
 
 # for Spyder. When you close your window, the QtApplicaiton instance is
 # still there after being created once. Therefore check if a Qt instance
@@ -123,58 +139,26 @@ b1.clicked.connect(qlewrapper)
 b2 = QtGui.QPushButton("Close port")
 b2.clicked.connect(close_port)
 
-t1 = QtGui.QLineEdit("Enter Device Serial")
+t1 = QtGui.QLineEdit("9553034373435110E020")
 
 p1 = pg.PlotWidget()
 p1.setRange(yRange=[-18000, 18000])
+p1.showGrid(x=True, y=True, alpha=0.5)
+p1.setLabel('left', 'Amplitude')
+p1.setLabel('bottom', 'Sample')
 p1.addLegend()
-p1.showGrid(x=True, y=True, alpha=0.8)
-p1.setLabel('left', 'Amplitude (16bit Signed)')
-
-curve1 = p1.plot(pen='y', name="Data 1")
-curve2 = p1.plot(pen='g', name="Data 2")
-curve3 = p1.plot()
 
 layout.addWidget(p1, 0, 0, 1, 3)
 layout.addWidget(b1, 1, 0)
 layout.addWidget(t1, 1, 1)
 layout.addWidget(b2, 1, 2)
 
-
-buffer1 = np.zeros(buffersize+1, int)
-buffer2 = np.zeros(buffersize+1, int)
-
 x = 0
-
-
-def update():
-    global curve1, curve2, x, ser, size, buffersize
-    if(ser != None and ser.is_open):
-        line = ser.readline()
-        csv = line.decode().split(',')
-        x += 1
-        if len(csv) == 2:
-
-            i = buffer1[buffersize]
-            buffer1[i] = buffer1[i+size] = csv[0]
-            buffer1[buffersize] = i = (i+1) % size
-
-            j = buffer2[buffersize]
-            buffer2[j] = buffer2[j+size] = csv[1]
-            buffer2[buffersize] = j = (j+1) % size
-
-            curve1.setData(buffer1[i:i+size])
-            curve1.setPos(x, 0)
-            curve2.setData(buffer2[j:j+size])
-            curve2.setPos(x, 0)
-            app.processEvents()
 
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
 timer.start(dt)
 timer.setInterval(dt)
-# if(ser != None):
-#  timer.stop()
 win.show()
 app.exec_()
